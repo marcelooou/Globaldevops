65144984d94836132b3ee1421b9af81a8faaafed
marcelooou <marcelosiqueirabonfim@gmail.com>
2025-11-11 22:21:14 -0300

    commit

---
+++ scripts/script-infra.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Script de Infra (Bash) para a Pipeline de Release (CD)
+# Foco: Criar os recursos da APLICAÇÃO (Oracle, Mongo)
+# Pré-requisito: O ACR ('acrskillmatchfiap') JÁ DEVE EXISTIR.
+
+echo "Iniciando script de infraestrutura (Bash)..."
+
+# --- Configurações ---
+RESOURCE_GROUP="Globalsolution"
+LOCATION="eastus2"
+
+# Senhas (NÃO COLOQUE SENHAS REAIS AQUI - Use Variáveis da Pipeline)
+# Nota: Em Bash, a sintaxe para variáveis é $VAR, e para strings é "string"
+ORACLE_PWD="password_forte_123"
+MONGO_USER="mongoadmin"
+MONGO_PWD="password_forte_456"
+
+# === 1. GARANTIR QUE O RESOURCE GROUP EXISTA (Requisito da GS, Pág 31) ===
+echo "Garantindo que o Resource Group '$RESOURCE_GROUP' exista..."
+az group create --name $RESOURCE_GROUP --location $LOCATION --output none
+
+# === 2. CRIAR OS BANCOS DE DADOS (Containers ACI) ===
+
+# A. Criar o Container do Oracle
+echo "Iniciando deploy do 'oracle-db-container'..."
+az container create --resource-group $RESOURCE_GROUP \
+    --name "oracle-db-container" \
+    --image "gvenzl/oracle-xe:latest" \
+    --ports 1521 \
+    --dns-name-label "oracle-skillmatch-eastus2" \
+    --environment-variables ORACLE_PASSWORD="$ORACLE_PWD" \
+    --cpu 1 --memory 2 \
+    --location $LOCATION \
+    --output none
+
+# B. Criar o Container do MongoDB
+echo "Iniciando deploy do 'mongo-db-container'..."
+az container create --resource-group $RESOURCE_GROUP \
+    --name "mongo-db-container" \
+    --image "mongo:latest" \
+    --ports 27017 \
+    --dns-name-label "mongo-skillmatch-eastus2" \
+    --environment-variables MONGO_INITDB_ROOT_USERNAME="$MONGO_USER" MONGO_INITDB_ROOT_PASSWORD="$MONGO_PWD" \
+    --cpu 1 --memory 1.5 \
+    --location $LOCATION \
+    --output none
+
+echo "Infraestrutura de containers (Bancos) criada com sucesso."
+echo "Oracle DNS: oracle-skillmatch-eastus2.$LOCATION.azurecontainer.io"
+echo "Mongo DNS: mongo-skillmatch-eastus2.$LOCATION.azurecontainer.io"
